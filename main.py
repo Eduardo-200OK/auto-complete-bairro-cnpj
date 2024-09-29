4ec757ec82cf2328118fdf1bb89888cff5dae7fc
Eduardo-200OK <rodrigues.e@tecpuc.com.br>
2024-09-29 19:38:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import CNPJextrator as ce
 import argparse
 
+
 def main():
+    parser = argparse.ArgumentParser(description="CNPJ para bairro")
 
-    paser = argparse.ArgumentParser(description="CPNJ para bairro")
+    parser.add_argument("file_path", type=str, help="Caminho da pasta para o xlsx. Ex: /home/docs/planilha1.xlsx")
+    parser.add_argument("output", type=str, help="Caminho de saida. Ex: /home/docs/saida1.xlsx")
+    parser.add_argument("column", type=str, help="Posicao da coluna em que se encontra o CNPJ. Ex: 3")
 
-    paser.add_argument("file_path", type=str, help="Caminho da pasta para o xlsx. Ex: /home/docs/planilha1.xlsx")
-    paser.add_argument("output", type=str, help="Caminho de saida. Ex: /home/docs/saida1.xlsx")
-    paser.add_argument("column", type=str, help="Posicao da coluna em que se encontra o CNPJ. Ex: 3")
-
-    args = paser.parse_args()
+    args = parser.parse_args()
 
     file_path = args.file_path
     output = args.output
     column = args.column
 
-    CNPJextrator = ce.CNPJExtrator(output=output, file_path=file_path, column=column)
-    CNPJextrator.execute()
+    cnpj_extrator = ce.CNPJExtrator(file_path=file_path, column=column, output=output)
+    cnpj_extrator.execute()
 
 if __name__ == '__main__':
     main()
